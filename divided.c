2a941736f606931f94a3236ee39858d3c8d17f7b
Vishu <vish7226058@gmail.com>
2025-05-19 16:30:54 +0530

    Add files via upload

---
+++ divided.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void main ()
+{
+	int x = 10 , y=20;
+
+    x= x*y;
+	y= x/y;
+	x= x/y;
+	
+	printf ("x=%d \n",x);
+	printf ("y= %d \n",y);	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
