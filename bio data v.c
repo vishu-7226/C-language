49fa7adc8f17af72510f93e8ccea7f1367a88a1d
Vishu <vish7226058@gmail.com>
2025-05-26 18:40:25 +0530

    Add files via upload

---
+++ bio data v.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf("Name : vishu yadav \n");
+
+	printf("age : 20 \n");
+   
+    printf("address : 469,rajdeep society dindoli,surat \n");
+	
+	printf("education: full stack learner \n");
+
+	printf("contact : 7226058520 \n");
+
+	printf("email : vish7226058@gmail.com \n");
+	
+
+}
