69a20a26f59698f6cc348718f8211bafc628b747
Vishu <vish7226058@gmail.com>
2025-05-30 18:24:52 +0530

    Add files via upload

---
+++ months number (1).c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main  ()
+{
+	int month;
+	printf ("enter month number :");
+	scanf ("%d",&month);
+	
+	if (month==1)
+	{
+		printf ("january ");
+	}
+	else if (month==2)
+	{
+		printf("febuary ");
+	}
+	else if (month==3)
+	{
+		printf (" march  ");
+	}
+	else if (month==4)
+	{
+		printf ("april ");
+    }
+    else if ( month==5)
+    {
+    	printf ("may");
+	}
+	
+	else if (month==6)
+	{
+		printf("may ");
+	}
+	else if (month==7)
+	{
+		printf (" june  ");
+	}
+	else if (month==8)
+	{
+		printf ("july");
+    }
+    else if ( month==9)
+    {
+    	printf ("august");
+	}
+	else if (month==10)
+	{
+		printf("september ");
+	}
+	else if (month==11)
+	{
+		printf (" november ");
+	}
+	else if (month==12)
+	{
+		printf ("december");
+    }
+    
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
